f85a0adbb4b252476ec53c02715b4679d730b7a6
martadomzalska <105391131+martadomzalska@users.noreply.github.com>
2023-09-27 16:20:18 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,53 @@
+import { useState } from 'react';
+import { Statistics } from './Statistics/Statistics';
+import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
+import { Section } from './Section/Section';
+
 export const App = () => {
+  const [feedback, setFeedback] = useState({
+    Good: 0,
+    Neutral: 0,
+    Bad: 0,
+  });
+
+  const handleLeaveMessage = e => {
+    const name = e.target.name;
+    setFeedback({
+      ...feedback,
+      [name]: feedback[name] + 1,
+    });
+  };
+
+  const countPositivePercentage = () => {
+    const { Good, Neutral, Bad } = feedback;
+    const total = Good + Neutral + Bad;
+    const positivePercentage = ((Good / total) * 100).toFixed(1);
+    if (Good > 0) {
+      return Number(positivePercentage);
+    } else {
+      return 0;
+    }
+  };
+
   return (
     <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
+     className='.container'
     >
-      React homework template
+      <Section title="Please leave feedback">
+        <FeedbackOptions
+          options={['Good', 'Neutral', 'Bad']}
+          onLeaveMessage={handleLeaveMessage}
+        />
+      </Section>
+      <Section title="Statistics"><Statistics
+        good={feedback.Good}
+        neutral={feedback.Neutral}
+        bad={feedback.Bad}
+        total={feedback.Good + feedback.Neutral + feedback.Bad}
+        positivePercentage={countPositivePercentage()}
+      ></Statistics></Section>
+
+      
     </div>
   );
 };
